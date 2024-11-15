2bddd89d728c8353b2b621b5178396aa53c05e57
Larisa Moșuț <larisa.mosut@emanuel.ro>
2024-11-15 12:20:46 +0200

    multiple pages in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ export default defineConfig({
    base: '',
    build:{
     rollupOptions: {
-      input: [ "./index.html", "./about.html"]
+      input: [ "./index.html", "./about.html","./team.html","./shop.html","./cart.html","./product1.html","./checkout.html","./faq.html","./blog.html","./contact.html",]
     }
   }
 })
